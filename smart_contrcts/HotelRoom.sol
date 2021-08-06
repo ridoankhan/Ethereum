62630be4c9f4f1f0fa4e6d020544822895d9484c
ridoankhan <ridoankhan07@gmail.com>
2021-08-07 03:21:20 +0600

    Hotel Room Contract added

---
+++ smart_contrcts/HotelRoom.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+contract HotelRoom {
+    enum Statuses {
+        Vacant,
+        Occupied
+    }
+    Statuses currentStatus;
+    event Occupy(address _occupant, uint256 _value);
+
+    address payable public owner;
+
+    constructor() {
+        owner = msg.sender;
+        currentStatus = Statuses.Vacant;
+    }
+
+    modifier onlyWhileVacant() {
+        require(currentStatus == Statuses.Vacant, "Currently occupied!");
+        _;
+    }
+
+    modifier hasPayedEnough(uint256 amount) {
+        require(amount >= amount, "Did not pay enough ether");
+        _;
+    }
+
+    receive() external payable {
+        currentStatus = Statuses.Occupied;
+        owner.transfer(msg.value);
+        emit Occupy(msg.sender, msg.value);
+    }
+}
